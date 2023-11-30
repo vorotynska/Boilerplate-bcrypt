9701c884746fad8d21ad450b35a38a843c36a863
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-11-30 17:10:22 +0100

    commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+const bcrypt = require('bcrypt');
 fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
@@ -10,11 +11,21 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+         console.log(hash)
+         /*Store hash in your db*/
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res)//true
+    /*res == true or false*/
+  });
+})
 
 
 //END_ASYNC
-
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+console.log(hash)
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log(result)
 //START_SYNC
 
 
